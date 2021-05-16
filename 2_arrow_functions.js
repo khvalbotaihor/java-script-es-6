2cd095b9c3c34292dcecf43eafff66fe032db26e
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2021-05-16 08:12:57 +0300

    update

--- 2_arrow_functions.js
+++ 2_arrow_functions.js
@@ -15,6 +15,4 @@ const cube = a => a ** 3
 // console.log(sum(2,3))
 // console.log(cube(3))
 
-setTimeout(() => {
-    console.log("After 1 second")
-},1000)
+setTimeout(() => console.log("After 1 second"),1000)
