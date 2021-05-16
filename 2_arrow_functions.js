a8c46f4a54c3cc0fbf03da058e15abd4dc064abb
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2021-05-16 08:09:17 +0300

    update

--- 2_arrow_functions.js
+++ 2_arrow_functions.js
@@ -1,3 +1,4 @@
+/*
 function sum(a, b) {
     return a + b
 }
@@ -5,3 +6,12 @@ function sum(a, b) {
 function cube(a) {
     return a ** 3
 }
+let a = sum(2,3)
+let b = cube(3)
+console.log({a, b})*/
+const sum = (a,b) => {
+    return a + b
+}
+const cube = a => a ** 3
+
+console.log(cube(3))
